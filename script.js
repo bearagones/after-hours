8212d7086a029439f5715bfbf7ab4251a6e065a2
bearagones <baragones@mail.sfsu.edu>
2024-11-25 00:30:53 -0800

    scroll to top button

--- script.js
+++ script.js
@@ -181,3 +181,17 @@ function getClosingTimes(record) {
 
     return formattedTimes.join('<br>'); // Return a formatted string
 }
+
+var mybutton = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block"; 
+    } else {
+        mybutton.style.display = "none"; 
+    }
+};
+
+mybutton.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
